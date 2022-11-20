e2e4764371a7c6d82f535857d32cc2be7c26060d
Arielle Dameshghi <adameshg@gmail.com>
2022-11-20 09:23:36 -0600

    welp...

---
+++ basics/FizzBuzz.java
@@ -0,0 +1,25 @@
+public class FizzBuzz {
+    public FizzBuzz() {}
+
+    public void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        fizzBuzz.fizzBuzz(10);
+    }
